97d9d69cbd2864f2cf3511835a1a26ee6ff8619e
Daniel Succar <danielsuccar@icloud.com>
2020-01-16 18:45:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,6 @@
 def using_push(colors_in_the_rainbow, next_color)
   updated_array = colors_in_the_rainbow.push(next_color)
-
 end
+
+def using_unshift(bouroughs_in_nyc,new_neighborhood)
+  using_unshift = bouroughs_in_nyc.unshift(new_neighborhood)
