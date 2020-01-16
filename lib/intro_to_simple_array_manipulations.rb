e40461f79ba2b2f892c579cbac9085559af71541
Daniel Succar <danielsuccar@icloud.com>
2020-01-16 19:46:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,7 +43,7 @@ def using_delete(instructors,no_offense_steven)
   using_delete = instructors.delete(no_offense_steven)
 end
 
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots,2)
   using_delete_at = famous_robots.delete_at(2)
 
 end
