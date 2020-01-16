234c52c6d67ea8ae95cf67ddc1f995e43faf6e6b
Daniel Succar <danielsuccar@icloud.com>
2020-01-16 19:30:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,7 @@ def using_concat (my_favorite_things, more_favs)
   using_concat = my_favorite_things.concat(more_favs)
 end
 
-def using_insert (list_of_programming_languages,@another_language)
+def using_insert (list_of_programming_languages,another_language)
   using_insert = list_of_programming_languages.insert(another_language)
 
 end
