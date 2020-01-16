2a72c5e96325453f35b409ca08ff3c381d26fcfe
Daniel Succar <danielsuccar@icloud.com>
2020-01-16 19:05:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,6 @@ def pop_with_args(dog_breeds)
   pop_with_args = dog_breeds.pop(2)
 end
 
-def using_shift(@my_favorite_cities)
+def using_shift(my_favorite_cities)
   using_shift = my_favorite_cities.shift
 end
